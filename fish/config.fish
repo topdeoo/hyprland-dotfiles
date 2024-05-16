8b1f45e69f81f81a89446216a12b947b036bb1e4
Virgil <virgiling7@gmail.com>
2024-05-16 20:35:29 +0800

    update: new config

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,11 @@ if status is-interactive
 end
 
 starship init fish | source
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda/bin/conda
+    eval /opt/anaconda/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
